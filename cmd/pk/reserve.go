a51a9118f31792ba7dd28110f154441ebfd51062
phanithken <phanith.ken@quad.co.jp>
2022-07-14 13:59:29 +0900

    fix: sanitize

--- cmd/pk/reserve.go
+++ cmd/pk/reserve.go
@@ -2,15 +2,16 @@ package pk
 
 import (
 	"fmt"
+
 	"github.com/phanithken/pk-cli/pkg/pk"
 	"github.com/spf13/cobra"
 )
 
 var reverseCmd = &cobra.Command{
-	Use: "reverse",
+	Use:     "reverse",
 	Aliases: []string{""},
-	Short: "Reverses a string",
-	Args: cobra.ExactArgs(1),
+	Short:   "Reverses a string",
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		res := pk.Reverse(args[0])
 		fmt.Println(res)
